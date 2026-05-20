1eb85190a3f6c71b06834eb4c0a0331c817a2879
Sara Valdéz <24sara.valdez@gmail.com>
2026-05-20 16:24:55 -0600

    Segundo Cambio

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,6 +1,75 @@
+const data = [
+    { id: 1, texto: 'Ganar en Among us', estaCompletada: true },
+    { id: 2, texto: 'Terminar la partida', estaCompletada: false }
+
+]
+
 let input = document.querySelector('#inputTarea')
 let btn = document.querySelector("#btnTarea")
 let tareas = document.querySelector('#tareas')
+
+if (data.length > 0) {
+
+    for (let i = 0; i <= data.length - 1; i++) {
+
+        //Dibujando Div de tarea
+        let div = document.createElement('div')
+        div.className = 'd-flex w-50 justify-content-evenly align-items-baseline'
+
+
+        //dibujando checkbox
+        let checkbox = document.createElement('input')
+        checkbox.setAttribute('type', 'checkbox')
+        checkbox.setAttribute('id', data[i].id)
+
+        checkbox.classList = 'me-2'
+
+
+        //dibujando el label
+        let label = document.createElement('label')
+        //pendiente: insertar el texto en el label
+
+        if (data[i].estaCompletada) {
+            checkbox.checked = true;
+            label.classList.add('text-decoration-line-through')
+        } else {
+            checkbox.checked = false;
+            label.classList.remove('text-decoration-line-through')
+        }
+
+        checkbox.addEventListener('click', (event) => {
+            //console.log(event.target.id)
+            let tareaABuscar = data.find(item => item.id == event.target.id)
+            tareaABuscar.estaCompletada = !tareaABuscar.estaCompletada
+
+            if (tareaABuscar.estaCompletada) {
+                label.classList.add('text-decoration-line-through')
+            }else{
+                label.classList.remove('text-decoration-line-through')
+            }
+        })
+
+
+        //dibujar el sup
+        let sup = document.createElement('sup')
+        sup.textContent = 'X'
+
+        label.textContent = data[i].texto
+
+        div.append(checkbox)
+        div.append(label)
+        div.append(sup)
+        //  input.value = ''
+        tareas.append(div)
+
+        sup.addEventListener('click', (event) => {
+            div.remove(checkbox, label, sup)
+        })
+
+    }
+}
+
+
 
 
 //Agregando elementos al div
@@ -38,14 +107,15 @@ btn.addEventListener('click', (event) => {
     div.append(checkbox)
     div.append(label)
     div.append(sup)
+
+    data.push({ id: data.length + 1, texto: input.value, estaCompletada: false })
+
     input.value = ''
     tareas.append(div)
-
 
     sup.addEventListener('click', (event) => {
         div.remove(checkbox, label, sup)
     })
 
 })
-
 
